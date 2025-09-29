d4af7b9840f68cb6e56eef266f65248fd578f0b9
barakavic <barakavictor397@gmail.com>
2025-09-29 23:09:41 +0300

    created a new fundraiser project but all the other things remain form the greeter project so in need of any insight on this one go to the greeter project's commits but the issue there was with the hardhat conflicts

---
+++ contracts/ffp1.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED 
+
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Fundraiser is Ownable{
+    string public name;
+    string public url;
+    string public imageURL;
+    string public description;
+    address payable public beneficiary;
+    address public custodian;
+
+
+
+    constructor(
+        string memory _name,
+        string memory _url,
+        string memory _imageURL,
+        string memory _description,
+        
+        address payable _beneficiary,
+        address _custodian
+    )
+    Ownable(_custodian)
+    {
+        name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        description = _description;
+        beneficiary = _beneficiary;
+        // _transferOwnership(_custodian);
+    }
+
+
+}
